4a02477ba6106f13cbd6c0ec05f55d2d17a327d1
kunalsonawane730 <95461743+kunalsonawane730@users.noreply.github.com>
2022-01-28 12:11:25 +0530

    [kunal]commiting cs programs

---
+++ repos/HelloworldTest/HelloworldTest/Program.cs
@@ -0,0 +1,8 @@
+﻿using HelloworldTest;
+FibonacciSeries series = new FibonacciSeries();
+series.FindFibonacciSeries();
+series.firstNum = 0;
+series.secondNum = 1;
+series.result = 0;
+ReverseNumber Reverse = new ReverseNumber();
+Reverse.FindReverse();
---
+++ repos/HelloworldTest/HelloworldTest/Reversenumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloworldTest
+{
+    public class ReverseNumber
+    {
+        public int numb = 2;
+        public static int first = 0;
+
+        public void FindReverse()
+        {
+            Console.WriteLine("Enter number to reverse");
+            int Number = int.Parse(Console.ReadLine());
+            int Reverse = 0;
+            Console.WriteLine(numb);
+            while (Number > 0)
+            {
+                int remainder = Number % 10;
+                Reverse = (Reverse * 10) + remainder;
+                Number = Number / 10;
+            }
+            Console.WriteLine("Reverse number is {0}", Reverse);
+            Console.ReadLine();
+        }
+
+    }
+}
